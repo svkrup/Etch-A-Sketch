39feb86378582662bdbd3861b42cf6a9c84f6251
svkrup <u362631@wns.com>
2023-03-23 21:48:19 +0530

    Regenerate grid based on number of cells provided by user

--- etch.js
+++ etch.js
@@ -1,9 +1,39 @@
 const master=document.querySelector('#master');
+const button=document.querySelector('#cellNum');
+let grid=16;
+//document.addEventListener('load',loadCells);
+document.onload=loadCells();
+button.addEventListener('click',()=>{
+    grid= prompt('Enter number of cells');
+    while(master.hasChildNodes()) {
+        master.removeChild(master.firstChild);
+    }
+    loadCells(); 
+})
+//let grid=16;
 
-for (let i=0;i<256;i++) {
-    const div=document.createElement('div');
-    div.setAttribute('id',i);
-    master.appendChild(div);
-};
+function loadCells(){
+    let cellSize=Math.round(1510/grid);
+    console.log(cellSize);
+    let side= `${cellSize}px`;
+    for (let i=0;i<grid*grid;i++) {
+        const div=document.createElement('div');
+        div.setAttribute('id',i);
+        div.style.width=side;
+        div.style.height=side;
+        div.addEventListener('mouseenter', function(e){
+            e.target.style.backgroundColor='red';
+        })
+        div.addEventListener('mouseleave', function(e){
+            e.target.style.backgroundColor='aliceblue'
+        })
+        master.appendChild(div);
+    };
+}
 
+let w = window.innerWidth;
+let h= window.innerHeight;
+console.log("width is "+ w);
+
+console.log("Hght is "+ h);
 
